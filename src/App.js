0bfad641eb785ae3df8b993e7485355956d5cddb
Kip-Hub <daniel.kip@hva.nl>
2022-01-17 14:14:03 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
+import { csv } from "d3";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [postcode, setPostcode] = useState("");
+  const [postcodeLink, setpostcodeLink] = useState(null);
+  const [cbsData, setcbsData] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      await fetch("./cbs.json")
+        .then((response) => response.json())
+        .then((data) => {
+          setcbsData(data);
+        });
+
+      const response = await csv("./areacodes.csv");
+      setpostcodeLink(response);
+    };
+
+    getData();
+  }, []);
+
+  const onSearch = () => {
+    const input = postcode.toUpperCase();
+    let areaCode = postcodeLink.filter((d) => d.PC6 === input)[0];
+    let combinedData = cbsData.filter((d) =>
+      d.GWB_CODE.includes(areaCode.Buurt2020)
+    );
+    console.log(combinedData);
+  };
+
+    return (
+        <section>
+        <p>Hoe gaat het er in jouw buurt aan toe?</p>
+          <input
+              placeholder="1234AB"
+              id="postcodeInput"
+              value={postcode}
+              onChange={(e) => setPostcode(e.target.value)}
+              />
+              <button onClick={onSearch}>Bekijk jouw buurt</button>  
+  </section>
+    )
 }
 
 export default App;
