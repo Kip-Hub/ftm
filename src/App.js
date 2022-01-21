c090f3a06d40595884da31b732f787994dc19649
mandemt <70643363+mandemt@users.noreply.github.com>
2022-01-21 10:31:50 +0100

    clean code and better functions

--- src/App.js
+++ src/App.js
@@ -27,45 +27,63 @@ function App() {
 
   const createList = () => {
 
-    results.forEach((data, index) => {
 
-      let propNames = Object.keys(data)
-      let dinges = propNames
+    function createNumbers(obj) {
+      results.forEach((data, index) => {
+        let fixed = data[obj].toString()
+        data[obj] = fixed.replace(/,/g, '.')
+        data[obj] = Number(data[obj])
+      })
 
-      data.Jaar = Number(data.Jaar)
-      let fixed = data.ziekenhuis_excl_afst.toString()
-      data.ziekenhuis_excl_afst = fixed.replace(/,/g, '.')
-      data.ziekenhuis_excl_afst = Number(data.ziekenhuis_excl_afst)
-      let yot = data.basis_afst.toString()
-      data.basis_afst = yot.replace(/,/g, '.')
-      data.basis_afst = Number(data.basis_afst)
+    }
 
+    function filterData(obj) {
+      createNumbers(obj)
+      const hoogste = Math.max.apply(Math, results.map(data => { return data[obj]; }))
+      const behorendJaar = results.filter(word => word[obj] === hoogste).map(function (o) {
+        return o.Jaar
+      })
+      return (
+        <>
+        <section>
+          
+          <p>De verste afstand van  naar een {obj} was <strong>{hoogste} </strong>kilometer.</p>
+          <p>Dit was in:
+            {behorendJaar.map(jaar => (
 
-    })
-    const highestValue = Math.max.apply(Math, results.map(data => { return data.ziekenhuis_excl_afst; }))
-    const highestSchoolValue = Math.max.apply(Math, results.map(function (o) { return o.basis_afst; }))
-    const filtered = results.filter(word => word.ziekenhuis_excl_afst === highestValue).map(function (o) {
-       console.log(results) 
-       return o.Jaar })
-    const basis = results.filter(word => word.basis_afst === highestSchoolValue).map(function (o) {
-      return o.Jaar
-    })
+              <strong> {jaar},</strong>
+            ))}
+          </p>
+          </section>
 
+        </>
+      )
 
-
+    }
 
     return (
       <>
+        {filterData('huisarts_afst')}
+        {filterData('ziekenhuis_excl_afst')}
+        {filterData('basis_afst')}
+        {filterData('bibliotheek_afst')}
+        {filterData('vmbo_afst')}
+        {filterData('havovwo_afst')}
+
+
+
         <table>
+
+
           <tr>
             <th>Plaats</th>
             <th>Jaar</th>
-            <th>Afstand huisarts</th>
+            <th>Afstand ziekenhuis</th>
             <th>Aantal inwoners</th>
+            <th>Afstand basisschool</th>
 
           </tr>
           {results.map((data, index) => (
-
             <tr>
               <td>{data.GWB_NAAM}</td>
               <td>{data.Jaar}</td>
@@ -77,12 +95,6 @@ function App() {
 
           ))}
 
-          <h1> yoo de verste ziekenhuis is {highestValue} in {filtered.map(jaar => (
-            <p>{jaar}</p>
-          ))}</h1>
-          <h2>yoo de verste basisschool is {highestSchoolValue} in{basis.map(jaar => (
-            <p>{jaar}</p>
-          ))} </h2>
 
         </table>
       </>
@@ -93,7 +105,6 @@ function App() {
 
     const input = postcode.toUpperCase();
     let areaCode = postcodeLink.filter((d) => d.PC6 === input)[0];
-
     let combinedData = cbsData.filter((d) =>
       d.GWB_CODE.includes(areaCode.Buurt2020)
     );
