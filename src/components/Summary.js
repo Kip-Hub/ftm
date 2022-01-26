890f70fb379d3d0e74b7b1e8ebc13ed811d265ef
Kip-Hub <daniel.kip@hva.nl>
2022-01-26 03:48:21 +0100

    added summary page with data

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -2,21 +2,70 @@ import React, { Component } from 'react';
 
 const Summary = (props) => {
 
-const firstYear = props.passedData[0].bibliotheek_afst.replaceAll(',', '.');
-const lastYear = props.passedData[props.passedData.length - 1].bibliotheek_afst.replaceAll(',', '.');
-let sum = lastYear - firstYear;
+const basisFirstYear = props.passedData[0].basis_afst.replaceAll(',', '.');
+const basisLastYear = props.passedData[props.passedData.length - 1].basis_afst.replaceAll(',', '.');
+let basisSum = Math.round(parseFloat(basisLastYear - basisFirstYear)*100)/100;
 
-console.log(props.passedData[0].Jaar + " " + firstYear);
-console.log(props.passedData[props.passedData.length - 1].Jaar + " " + lastYear);
+const vmboFirstYear = props.passedData[0].basis_afst.replaceAll(',', '.');
+const vmboLastYear = props.passedData[props.passedData.length - 1].basis_afst.replaceAll(',', '.');
+let vmboSum = Math.round(parseFloat(vmboLastYear - vmboFirstYear)*100)/100;
+
+const havovwoFirstYear = props.passedData[0].havovwo_afst.replaceAll(',', '.');
+const havovwoLastYear = props.passedData[props.passedData.length - 1].havovwo_afst.replaceAll(',', '.');
+let havovwoSum = Math.round(parseFloat(havovwoLastYear - havovwoFirstYear)*100)/100;
+
+const bibFirstYear = props.passedData[0].bibliotheek_afst.replaceAll(',', '.');
+const bibLastYear = props.passedData[props.passedData.length - 1].bibliotheek_afst.replaceAll(',', '.');
+let bibSum = Math.round(parseFloat(bibLastYear - bibFirstYear)*100)/100;
+
+const ziekFirstYear = props.passedData[0].ziekenhuis_incl_afst.replaceAll(',', '.');
+const ziekLastYear = props.passedData[props.passedData.length - 1].ziekenhuis_incl_afst.replaceAll(',', '.');
+let ziekSum = Math.round(parseFloat(ziekLastYear - ziekFirstYear)*100)/100;
+
+
 
 return (
     <div class ="storycontainer">
-        <div>Hoe gaat het eraan toe in <p class="highlighted">{props.passedData[0].GWB_NAAM}</p></div>
-        <div>Afstand basisschool: <p></p></div>
-        <div>Afstand vmbo: <p></p></div>
-        <div>Afstand havo/vwo: <p></p></div>
-        <div>Afstand bibliotheek: <p></p></div>
-        <div>Afstand ziekenhuis: <p></p></div>
+        <h2>Hoe gaat het eraan toe in <span class="highlighted">{props.passedData[0].GWB_NAAM}</span></h2>
+        <div class ="storycontent">
+            <p>Afstand basisschool: {(() => {
+                switch (basisSum <= 0) {
+                    case true:   return <span class="green"> {basisSum.toString().replaceAll('-','')} km</span>;
+                    case false: return <span class="red"> {basisSum} km</span>;
+                }
+            })()}</p>
+                
+            <p>Afstand vmbo: {(() => {
+                switch (vmboSum <= 0) {
+                    case true:   return <span class="green"> {vmboSum.toString().replaceAll('-','')} km</span>;
+                    case false: return <span class="red"> {vmboSum} km</span>;
+                }
+            })()}</p>
+
+            <p>Afstand havo/vwo: {(() => {
+                switch (havovwoSum <= 0) {
+                    case true:   return <span class="green"> {havovwoSum.toString().replaceAll('-','')} km</span>;
+                    case false: return <span class="red"> {havovwoSum} km</span>;
+                }
+            })()}</p>
+
+            <p>Afstand bibliotheek: {(() => {
+                switch (bibSum <= 0) {
+                    case true:   return <span class="green"> {bibSum.toString().replaceAll('-','')} km</span>;
+                    case false: return <span class="red"> {bibSum} km</span>;
+                }
+            })()}</p>
+
+            <p>Afstand ziekenhuis: {(() => {
+                switch (ziekSum <= 0) {
+                    case true:   return <span class="green"> {ziekSum.toString().replaceAll('-','')} km</span>;
+                    case false: return <span class="red"> {ziekSum} km</span>;
+                }
+            })()}</p>
+            <p class="sidenote"> <span class="green">groen</span> : niet veranderd / verkleind </p>
+            <p class="sidenote"> <span class="red">rood</span> : afstand vergroot </p>
+        </div>
+        
     </div>
 );
 }
